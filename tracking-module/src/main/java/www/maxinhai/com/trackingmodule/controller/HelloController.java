0f4f004f03c2fcdd8d9078c7df588a714d0100c9
mxh1997java <18334698204@163.com>
2020-07-22 20:55:36 +0800

    提交代码

--- tracking-module/src/main/java/www/maxinhai/com/trackingmodule/controller/HelloController.java
+++ tracking-module/src/main/java/www/maxinhai/com/trackingmodule/controller/HelloController.java
@@ -5,30 +5,21 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import www.maxinhai.com.trackingshare.appservice.HelloService;
-import java.util.HashMap;
 import java.util.Map;
 
 @RequestMapping("tracking")
 @RestController
-public class HelloController implements HelloService {
+public class HelloController extends AbstractController implements HelloService {
 
     @RequestMapping(value = "/hello", method = {RequestMethod.GET, RequestMethod.POST})
     @Override
     public Map<String, Object> hello(@RequestParam("name") String name) throws Exception {
-        Map<String, Object> result = new HashMap<>();
-        result.put("message", "tracking模块");
-        result.put("success", true);
-        result.put("code", 200);
-        return result;
+        return getSuccess("tracking模块");
     }
 
     @RequestMapping(value = "/test", method = {RequestMethod.GET, RequestMethod.POST})
     @Override
     public Map<String, Object> test() throws Exception {
-        Map<String, Object> result = new HashMap<>();
-        result.put("message", "tracking模块");
-        result.put("success", true);
-        result.put("code", 200);
-        return result;
+        return getSuccess("tracking模块");
     }
 }
